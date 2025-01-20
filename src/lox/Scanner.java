eceef533e123fbdde9b6c398292acf2eed1ddd4f
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2025-01-20 22:16:15 +0700

    Longer Lexemes

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -35,7 +35,88 @@ class Scanner {
 			case '+': addToken(TokenType.PLUS); break;
 			case ';': addToken(TokenType.SEMICOLON); break;
 			case '*': addToken(TokenType.STAR); break;
+			case '!': addToken(match('=') ? TokenType.BANG_EQUAL : TokenType.BANG); break;
+			case '=': addToken(match('=') ? TokenType.EQUAL_EQUAL : TokenType.EQUAL); break;
+			case '<': addToken(match('=') ? TokenType.LESS_EQUAL : TokenType.LESS); break;
+			case '>': addToken(match('=') ? TokenType.GREATER_EQUAL : TokenType.GREATER); break;
+			case '/': 
+				if (match('/')) {
+					// A comment goes until the end of the line
+					while (peek() != '\n' && !isAtEnd()) advance();
+				} else {
+					addToken(TokenType.SLASH);
+				}
+				break;
+			case ' ':
+			case '\r':
+			case '\t':
+				// Ignore whitespace
+				break;
+			case '\n': line++; break;
+			case '"': string(); break;
+			default :
+				if (isDigit(c)) {
+					number();
+				} else {
+					Lox.error(line, "Unexpected character.");
+				}
+				break;
 		}
+	}
+
+	private void number() {
+		while (isDigit(peek())) advance();
+
+		// Look for a fractional part.
+		if (peek() == '.' && isDigit(peekNext())) {
+			// Consume the "."
+			advance();
+
+			while (isDigit(peek())) advance();
+		}
+
+		addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+
+	private void string() {
+		while (peek() != '"' && !isAtEnd()) {
+			if (peek() == '\n') line++;
+			advance();
+		}
+
+		if (isAtEnd()) {
+			Lox.error(line, "Unterminated string.");
+			return;
+		}
+
+		// The closeing ".
+		advance();
+
+		// Trim the surrounding quotes.
+		String value = source.substring(start + 1, current - 1);
+		addToken(TokenType.STRING, value);
+	}
+
+	private boolean match(char expected) {
+		if (isAtEnd()) return false;
+		if (source.charAt(current) !=  expected) return false;
+
+		current++;
+		return true;
+	}
+
+	private char peek() {
+		if (isAtEnd()) return '\0';
+		return source.charAt(current);
+	}
+
+	private char peekNext() {
+		if (current + 1 >= source.length()) return '\0';
+		return source.charAt(current + 1);
+	}
+
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
 	}
 
 	private boolean isAtEnd() {
