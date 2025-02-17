1c88847286ddf8630223b4c26e85c7406b647d11
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2025-02-17 22:08:28 +0700

    Wiring up the parser

--- src/com/lox/Parser.java
+++ src/com/lox/Parser.java
@@ -14,6 +14,14 @@ class Parser {
 		this.tokens = tokens;
 	}
 
+	Expr parse() {
+		try {
+			return expression();
+		} catch (ParseError error) {
+			return null;
+		}
+	}
+
 	private Expr expression() {
 		return equality();
 	}
@@ -93,6 +101,8 @@ class Parser {
 			consume(RIGHT_PAREN, "Exprect ')' after expression.");
 			return new Expr.Grouping(expr);
 		}
+
+		throw error(peek(), "Expect expression.");
 	}
 
 	private boolean match(TokenType... types) {
@@ -139,5 +149,27 @@ class Parser {
 	private ParseError error(Token token, String message) {
 		Lox.error(token, message);
 		return new ParseError();
+	}
+
+	private void synchronize() {
+		advance();
+
+		while (!isAtEnd()) {
+			if (previous().type == SEMICOLON) return;
+
+			switch (peek().type) {
+				case CLASS:
+				case FUN:
+				case VAR:
+				case FOR:
+				case IF:
+				case WHILE:
+				case PRINT:
+				case RETURN:
+					return;
+			}
+
+			advance();
+		}
 	}
 }
