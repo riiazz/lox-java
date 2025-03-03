072d1913f738d011a57732150f04514016c61d30
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2025-03-03 21:48:31 +0700

    Statements

--- src/com/lox/Interpreter.java
+++ src/com/lox/Interpreter.java
@@ -1,16 +1,21 @@
 package com.lox;
 
+import java.util.List;
 import com.lox.Expr.Binary;
 import com.lox.Expr.Grouping;
 import com.lox.Expr.Literal;
 import com.lox.Expr.Unary;
+import com.lox.Stmt.Expression;
+import com.lox.Stmt.Print;
 
-public class Interpreter implements Expr.Visitor<Object> {
+public class Interpreter implements Expr.Visitor<Object>, 
+				    Stmt.Visitor<Void> {
 
-	void interpret(Expr expression) {
+	void interpret(List<Stmt> statements) {
 		try {
-			Object value = evaluate(expression);
-			System.out.println(stringify(value));
+			for (Stmt statement : statements) {
+				execute(statement);
+			}
 		} catch (RuntimeError error) {
 			Lox.runtimeError(error);
 		}
@@ -127,5 +132,22 @@ public class Interpreter implements Expr.Visitor<Object> {
 
 	private Object evaluate(Expr expr) {
 		return expr.accept(this);
+	}
+
+	private void execute(Stmt stmt) {
+		stmt.accept(this);
+	}
+
+	@Override
+	public Void visitExpressionStmt(Expression stmt) {
+		evaluate(stmt.expression);
+		return null;
+	}
+
+	@Override
+	public Void visitPrintStmt(Print stmt) {
+		Object value = evaluate(stmt.expression);
+		System.out.println(stringify(value));
+		return null;
 	}
 }
